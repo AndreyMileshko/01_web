7478de9f76195c49194f7a4fbf03d937bd90ecb4
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-09-12 14:07:50 +0700

    Task prefiling

--- http-server/src/main/java/ru/netology/server/Server.java
+++ http-server/src/main/java/ru/netology/server/Server.java
@@ -3,12 +3,16 @@ package ru.netology;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static ru.netology.RequestHandler.*;
 
 public class Server {
+
+    private final Map<String, Map<String, Handler>> handlers = new HashMap<>();
     private final int port;
     private final ExecutorService threadPool;
     private boolean cont = true;
@@ -27,6 +31,11 @@ public class Server {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        handlers.put(method, new HashMap<>());
+
     }
 
     public void setCont(boolean cont) {
