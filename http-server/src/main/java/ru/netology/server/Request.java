880cd145007893683b4b79354d5d1e27026cd8bf
Andrey Mileshko <AndreyPrograming.Mileshko@yandex.ru>
2024-09-24 14:44:19 +0700

    queryString parsing is implemented

--- http-server/src/main/java/ru/netology/server/Request.java
+++ http-server/src/main/java/ru/netology/server/Request.java
@@ -1,17 +1,25 @@
 package ru.netology.server;
 
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URLEncodedUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Request {
     private final String method;
     private final String path;
     private final BufferedReader in;
+    private final List<NameValuePair> queryParams;
 
-    public Request(String method, String path, BufferedReader in) {
+    private Request(String method, String path, BufferedReader in, List<NameValuePair> queryParams) {
         this.method = method;
         this.path = path;
         this.in = in;
+        this.queryParams = queryParams;
     }
 
     public String getMethod() {
@@ -34,7 +42,26 @@ public class Request {
             return null;
 
         final String method = parts[0];
-        final String path = parts[1];
-        return new Request(method, path, in);
+        final String pathAndQueryString = parts[1];
+
+        if (!pathAndQueryString.contains("?")) {
+            return new Request(method, pathAndQueryString, in, null);
+        }
+
+        final String[] pathAndQueryStringArray = pathAndQueryString.split("\\?");
+        final String path = pathAndQueryStringArray[0];
+        final String queryString = pathAndQueryStringArray[1];
+        List<NameValuePair> queryParams = URLEncodedUtils.parse(queryString, Charset.defaultCharset());
+        return new Request(method, path, in, queryParams);
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public List<NameValuePair> getQueryParam(String name) {
+        return queryParams.stream()
+                .filter(str -> str.toString().contains(name))
+                .collect(Collectors.toList());
     }
 }
